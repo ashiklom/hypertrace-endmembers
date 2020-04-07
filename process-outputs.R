e74e6165402bde98c882306c37d7cf707efdfe40
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-04-07 10:15:59 -0400

    Minor tweaks to process-outputs script

--- process-outputs.R
+++ process-outputs.R
@@ -5,21 +5,14 @@ library(purrr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+library(here)
 
-datadir <- dir_create("data")
+figdir <- dir_create("figures/endmembers-2")
 
-## # Refresh the endmember file
-## url <- paste0(
-##   "https://files.osf.io/v1/resources/kp82c/providers/googledrive/",
-##   "SSerbin_hypertrace_results/instrument_models/kougarok_ang20180815t203458_rfl_v2r2/",
-##   "?zip="
-## )
-## outfile <- path(datadir, "endmembers.zip")
-## download.file(url, outfile)
-## unzip(outfile, exdir = datadir)
-
-statfiles <- dir_ls("data", type = "directory") %>%
-  dir_ls(glob = "*statistics.csv")
+statfiles <- here("data", "endmembers_v2") %>%
+  list.files(pattern = "statistics.csv", recursive = TRUE,
+             full.names = TRUE)
+names(statfiles) <- statfiles
 
 statcols <- cols(
   inversion = "c",
@@ -31,8 +24,7 @@ statcols <- cols(
 dat_raw <- map_dfr(statfiles, read_csv, .id = "file", col_types = statcols)
 
 dat <- dat_raw %>%
-  mutate(parentdir = path_file(path_dir(file))) %>%
-  extract(parentdir, "endmember", "^(.*)_endmembers_.*")
+  extract(file, "endmember", ".*_rfl_v2r2_(.*)_endmembers_.*")
 
 outliers <- dat %>%
   filter(r < 0.75)
@@ -59,7 +51,7 @@ plt <- ggplot(dat_long) +
   ##   dodge.width = 0.8
   ## ) +
   facet_grid(vars(variable), scales = "free_y")
-figdir <- dir_create("figures")
+
 ggsave(
   path(figdir, "endmembers.png"),
   plt,
@@ -70,7 +62,7 @@ plt2 <- ggplot(anti_join(dat_long, outliers)) +
   aes(x = snr, y = value, fill = endmember) +
   geom_boxplot(outlier.shape = NA) +
   ggbeeswarm::geom_quasirandom(
-    cex = 0.2,
+    alpha = 0.5, cex = 0.2,
     dodge.width = 0.8
   ) +
   facet_grid(vars(variable), scales = "free_y")
@@ -93,7 +85,7 @@ byatm <- dat_long %>%
   facet_grid(vars(variable), vars(atm), scales = "free_y")
 
 ggsave(
-  "figures/endmembers-nooutliers-byatm.png",
+  path(figdir, "endmembers-nooutliers-byatm.png"),
   byatm,
   width = 21, height = 11.5, dpi = 200
 )
